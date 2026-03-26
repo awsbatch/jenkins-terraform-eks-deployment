2fc21b45c51b6913e3e6d2521f0bb56a34babb0c
ravendra rana <ravendra.rana@vfirst.com>
2026-03-26 14:26:37 +0530

    add jenkinsfile

---
+++ jenkinsfile.groovy
@@ -0,0 +1,42 @@
+pipeline {
+    agent any
+
+    environment {
+        AWS_ACCESS_KEY_ID     = credentials('aws_access_key_id')
+        AWS_SECRET_ACCESS_KEY = credentials('aws_secret_access_key')
+        AWS_DEFAULT_REGION    = 'us-east-1'
+    }
+
+    stages {
+        stage('Clone Repo') {
+            steps {
+                git branch: 'main',
+                    url: 'https://github.com/awsbatch/jenkins-terraform-eks-deployment.git'
+            }
+        }
+        stage('Terraform init') {
+            steps {
+                dir('EKS') {
+                sh '''
+                rm -rf .terraform .terraform.lock.hcl
+                terraform init
+                '''
+                }
+            }
+        }
+        stage('Terraform fmt') {
+            steps {
+                dir('EKS') {
+                    sh 'terraform fmt'
+                }
+            }
+        }
+        stage('Terraform plan') {
+            steps {
+                dir('EKS') {
+                    sh 'terraform plan'
+                }
+            }
+        }
+}
+}
